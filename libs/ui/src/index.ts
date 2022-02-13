8ac05babbfed2353486daae49ee1655f3d215a3c
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-13 18:52:50 +0100

    feature/add-common-ui

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/ui.module';
+export * from './lib/yt-brand-icon/yt-brand-icon.module';
---
+++ libs/ui/src/lib/yt-brand-icon/yt-brand-icon.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { YtIconComponent } from "./yt-brand-icon.component";
+
+@NgModule({
+    declarations: [YtIconComponent],
+    exports: [YtIconComponent]
+  })
+export class YTIconModule {
+}
