e8e7f95c2341c7d3600055f847293255306ccf9f
Vugar <vugar.005.va@gmail.com>
2022-02-13 18:36:03 +0100

    Feature/add shell app (#1)
    * add nrwl angular plugin
    
    * feature/add-shell-app
    
    * add header
    
    * add angular material
    
    * add sidebar
    
    * add material theme
    
    * add sidabar initial version and google fonts
    
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

---
+++ apps/shell/src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'yt-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Output() readonly openMenu = new EventEmitter<void>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onOpenMenu(): void {
+    console.log('openm')
+    this.openMenu.next();
+  }
+
+}
---
+++ apps/shell/webpack.config.js
@@ -0,0 +1,87 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const mf = require('@angular-architects/module-federation/webpack');
+const path = require('path');
+const share = mf.share;
+
+/**
+ * We use the NX_TSCONFIG_PATH environment variable when using the @nrwl/angular:webpack-browser
+ * builder as it will generate a temporary tsconfig file which contains any required remappings of
+ * shared libraries.
+ * A remapping will occur when a library is buildable, as webpack needs to know the location of the
+ * built files for the buildable library.
+ * This NX_TSCONFIG_PATH environment variable is set by the @nrwl/angular:webpack-browser and it contains
+ * the location of the generated temporary tsconfig file.
+ */
+const tsConfigPath =
+  process.env.NX_TSCONFIG_PATH ??
+  path.join(__dirname, '../../tsconfig.base.json');
+
+const workspaceRootPath = path.join(__dirname, '../../');
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  tsConfigPath,
+  [
+    /* mapped paths to share */
+  ],
+  workspaceRootPath
+);
+
+module.exports = {
+  output: {
+    uniqueName: 'shell',
+    publicPath: 'auto',
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  experiments: {
+    outputModule: true,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      remotes: {},
+      shared: share({
+        '@angular/core': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        '@angular/common': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        '@angular/common/http': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        '@angular/router': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        rxjs: {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        ...sharedMappings.getDescriptors(),
+      }),
+      library: {
+        type: 'module',
+      },
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
