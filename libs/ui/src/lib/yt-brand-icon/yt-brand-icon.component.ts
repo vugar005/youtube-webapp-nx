8a64a5f316230514c8dcb753f0f755fa360cefad
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-13 19:39:23 +0100

    Feature/add onpush default schematics (#4)
    * feature/add-onpush-default-schematics
    
    * add default schematics to common ui
    
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- libs/ui/src/lib/yt-brand-icon/yt-brand-icon.component.ts
+++ libs/ui/src/lib/yt-brand-icon/yt-brand-icon.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'yt-brand-icon',
   templateUrl: './yt-brand-icon.component.html',
   styleUrls: ['./yt-brand-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class YtIconComponent {
   @Input() width?: string | number;
