c47783055c31a5e303ce8614ab8835ec067a3463
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-13 16:32:54 +0100

    add header

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
