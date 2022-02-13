52c4ce57a65e2ce7cdb4cb91781d0d401b44d839
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-13 18:36:03 +0100

    Feature/add shell app (#1)
    * add nrwl angular plugin
    
    * feature/add-shell-app
    
    * add header
    
    * add angular material
    
    * add sidebar
    
    * add material theme
    
    * add sidabar initial version and google fonts
    
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

---
+++ apps/shell/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app.routing';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, SidebarComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
