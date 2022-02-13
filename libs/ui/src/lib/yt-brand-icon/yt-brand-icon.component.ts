94bb6e2e47be8aeff28263c92fd72159f242b2e4
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-13 18:52:50 +0100

    feature/add-common-ui

---
+++ libs/ui/src/lib/yt-brand-icon/yt-brand-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'yt-brand-icon',
+  templateUrl: './yt-brand-icon.component.html',
+  styleUrls: ['./yt-brand-icon.component.scss']
+})
+export class YtIconComponent {
+  @Input() width?: string | number;
+
+}
