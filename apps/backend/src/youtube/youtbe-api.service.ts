ccbb97684c35fa6a12caf4086f84f511a0325131
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-15 22:39:49 +0100

    Feature/watch app dynamic contents (#15)
    * feature/watch-app-dynamic-contents
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- apps/backend/src/youtube/youtbe-api.service.ts
+++ apps/backend/src/youtube/youtbe-api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { IYoutubeSearchResult } from '@youtube/common-ui';
 import * as yt from 'youtube-search-without-api-key';
 
 @Injectable()
 export class YoutubeApiService {
-  public async searchVideoResults(query: string) {
+  public async searchVideoResults(query: string): Promise<IYoutubeSearchResult[]> {
     const results = await yt.search(query);
     return results;
   }
--- apps/watch-app/src/app/related-videos/related-videos.component.ts
+++ apps/watch-app/src/app/related-videos/related-videos.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { SEARCH_RESULT_MOCK } from '@youtube/common-ui';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Inject, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
+import { IYoutubeSearchResult, IYoutubeService, YOUTUBE_SERVICE } from '@youtube/common-ui';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'watch-app-related-videos',
@@ -7,9 +8,35 @@ import { SEARCH_RESULT_MOCK } from '@youtube/common-ui';
   styleUrls: ['./related-videos.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RelatedVideosComponent implements OnInit {
-  public resultMock = SEARCH_RESULT_MOCK;
-  constructor() {}
+export class RelatedVideosComponent implements OnInit, OnChanges {
+  @Input() query!: string;
+  @Input() relatedVideos?: IYoutubeSearchResult[];
 
-  ngOnInit(): void {}
+  constructor(
+    @Inject(YOUTUBE_SERVICE) private youtubeService: IYoutubeService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  public ngOnInit(): void {
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+      const queryChange = changes && changes['query'];
+      if (queryChange)  {
+       this.getRelatedVideos();
+      }
+  }
+
+  private getRelatedVideos(): void {
+    this.youtubeService.searchVideoResults({query: this.query?.slice(0, 7)})
+    .pipe(
+      filter(results => !!results?.length)
+    )
+      .subscribe((results: IYoutubeSearchResult[]) => {
+        this.relatedVideos = results;
+        this.cdr.detectChanges();
+      });
+  }
 }
+
+
