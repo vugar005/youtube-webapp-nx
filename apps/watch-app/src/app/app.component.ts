931444d446b8ab0a547a30e21414e61af1c54962
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-15 14:26:23 +0100

    feature/connect-shell-to-watch-app

--- apps/watch-app/src/app/app.component.ts
+++ apps/watch-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'watch-app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'watch-app';
+  constructor(private router: Router) {}
+
+  public ngOnInit(): void {
+    this.connectRouter();
+  }
+
+  private connectRouter(): void {
+    const url = `${location.pathname.substr(1)}${location.search}`;
+    this.router.navigateByUrl(url);
+    window.addEventListener('popstate', () => {
+      this.router.navigateByUrl(url);
+    });
+  }
 }
--- apps/watch-app/src/app/app.module.ts
+++ apps/watch-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
 import { RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
-export class AppModule {}
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+
+  public ngDoBootstrap(): void {
+    const ce = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('watch-app-element', ce);
+    // <watch-app-element></watch-app-element>
+  }
+}
