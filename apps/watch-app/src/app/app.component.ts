b744c5c330f09ffe76f7e5af94094e728d75f204
Vugar <vugar_abdullayev@outlook.com>
2022-02-15 14:03:07 +0100

    Feature/add watch app (#9)
    * feature/add-watch app
    
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

---
+++ apps/watch-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'watch-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'watch-app';
+}
