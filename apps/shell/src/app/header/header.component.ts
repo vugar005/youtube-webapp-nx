62e67bf6ad60a86910889b9a26594cd22e7f011c
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-17 23:43:53 +0100

    feature/add-theme-change-feature

--- apps/shell/src/app/header/header.component.ts
+++ apps/shell/src/app/header/header.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
+import { SettingsStore } from '../core/services/settings-store/settings-store.service';
+import { AppTheme } from '../core/services/theme-service/theme.constants';
+import { ThemeService } from '../core/services/theme-service/theme.service';
 import { VideoStoreService } from '../core/services/video-store/video-store.service';
 
 @Component({
@@ -17,7 +20,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private readonly onDestroy$ = new Subject<void>();
 
-  constructor(private videoStore: VideoStoreService, private router: Router) {}
+  constructor(
+    private videoStore: VideoStoreService,
+    private router: Router,
+    private settingsStore: SettingsStore,
+    private themeService: ThemeService
+    ) {}
 
   public ngOnInit(): void {
     this.listenToEvents();
@@ -34,6 +42,22 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   public onToggleAccountMenu(): void {
     this.toggleAccountMenu.next();
+  }
+
+  public onChangeTheme(): void {
+    this.settingsStore.selectTheme()
+    .pipe(
+      take(1)
+    ).subscribe((theme: AppTheme | null) => {
+      console.log(theme);
+        if (theme === AppTheme.DARK) {
+          console.log('set light')
+          this.themeService.setTheme(AppTheme.LIGHT)
+        } else {
+          console.log('set dark')
+          this.themeService.setTheme(AppTheme.DARK);
+        }
+    });
   }
 
   private listenToEvents(): void {
