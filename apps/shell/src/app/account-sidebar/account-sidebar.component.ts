624f9e3bc548161bba7e43d87cb322539b2ffa78
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-17 23:48:19 +0100

    Feature/add theme change feature (#27)
    * feature/add-theme-change-feature
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- apps/shell/src/app/account-sidebar/account-sidebar.component.ts
+++ apps/shell/src/app/account-sidebar/account-sidebar.component.ts
@@ -1,5 +1,4 @@
-import { ViewportScrollPosition } from '@angular/cdk/scrolling';
-import { Component, OnInit, ChangeDetectionStrategy, VERSION } from '@angular/core';
+import { Component, ChangeDetectionStrategy, VERSION } from '@angular/core';
 
 @Component({
   selector: 'yt-account-sidebar',
@@ -7,10 +6,6 @@ import { Component, OnInit, ChangeDetectionStrategy, VERSION } from '@angular/co
   styleUrls: ['./account-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountSidebarComponent implements OnInit {
+export class AccountSidebarComponent {
   public readonly version = VERSION.full;
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
--- apps/shell/src/app/reducers.ts
+++ apps/shell/src/app/reducers.ts
@@ -1,17 +1,20 @@
 import { Action, ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromVideo from './core/reducers/video.reducer';
 import * as fromAccount from './core/reducers/account.reducer';
+import * as fromSettings from './core/reducers/settings.reducer';
 import { InjectionToken } from '@angular/core';
 
 export interface AppState {
   [fromVideo.featureKey]: fromVideo.VideoState;
   [fromAccount.featureKey]: fromAccount.AccountState;
+  [fromSettings.featureKey]: fromSettings.SettingsState;
 }
 
 export const ROOT_REDUCERS = new InjectionToken<ActionReducerMap<AppState, Action>>('Root reducers token', {
   factory: () => ({
     [fromVideo.featureKey]: fromVideo.reducer,
     [fromAccount.featureKey]: fromAccount.reducer,
+    [fromSettings.featureKey]: fromSettings.reducer,
   }),
 });
 
@@ -23,3 +26,7 @@ export const selectVideoSearchQuery = createSelector(selectVideoState, fromVideo
 export const selectAccountState = createFeatureSelector<fromAccount.AccountState>(fromAccount.featureKey);
 export const selectLikedVideos = createSelector(selectAccountState, fromAccount.selectLikedVideos);
 export const selectDislikedVideos = createSelector(selectAccountState, fromAccount.selectDislikedVideos);
+
+// Settings state selectors
+export const selectSettingsState = createFeatureSelector<fromSettings.SettingsState>(fromSettings.featureKey);
+export const selectTheme = createSelector(selectSettingsState, fromSettings.selectSettingsTheme);
