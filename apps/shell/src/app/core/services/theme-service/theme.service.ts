27ddd5e14eb344807b17768da622bb4f170f56fe
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-18 12:54:26 +0100

    feature/add-dom-tokens

--- apps/shell/src/app/core/services/theme-service/theme.service.ts
+++ apps/shell/src/app/core/services/theme-service/theme.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { WebApiService } from '@youtube/common-ui';
 import { SettingsStore } from '../settings-store/settings-store.service';
 import { AppTheme } from './theme.constants';
 
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
-  constructor(private settingsStore: SettingsStore) {}
+  constructor(
+    private settingsStore: SettingsStore,
+    private webApi: WebApiService
+    ) {}
 
   public setTheme(theme: AppTheme): void {
-    const root = document.getElementsByTagName('html')[0];
+    let root = this.webApi.document.getElementsByTagName('html')[0];
     root.setAttribute('theme', theme);
     this.settingsStore.setTheme(theme);
   }
--- libs/ui/src/lib/services/index.ts
+++ libs/ui/src/lib/services/index.ts
@@ -1,3 +1,4 @@
 export * from './youtube-service';
 export * from './youtube-v2-service';
 export * from './events-dispatcher';
+export * from './web-api';
