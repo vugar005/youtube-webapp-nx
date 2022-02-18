0054f3de1f19c5548bff51a13913bc98713e8d08
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-18 16:58:26 +0100

    feature/improve-video-player-ui

--- libs/ui/src/lib/video-thumbnail-loader/video-thumbnail-loader.component.ts
+++ libs/ui/src/lib/video-thumbnail-loader/video-thumbnail-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'yt-video-thumbnail-loader',
@@ -7,6 +7,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VideoThumbnailLoaderComponent implements OnInit {
+  @Input() direction?: 'vertical' | 'horizontal';
   constructor() {}
 
   ngOnInit(): void {}
--- libs/ui/src/lib/video-thumbnail-loader/video-thumbnail-loader.module.ts
+++ libs/ui/src/lib/video-thumbnail-loader/video-thumbnail-loader.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AspectRatioDirectiveModule } from '../directives';
 
 import { VideoThumbnailLoaderComponent } from './video-thumbnail-loader.component';
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AspectRatioDirectiveModule],
   declarations: [VideoThumbnailLoaderComponent],
   exports: [VideoThumbnailLoaderComponent],
 })
