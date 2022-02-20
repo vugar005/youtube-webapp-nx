87addc2688596c04ea213de3839c2a7cd9848db3
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-20 22:00:00 +0100

    Feature/add apps firebase config (#36)
    * add firebase config and prod app urls
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- apps/shell/src/environments/environment.prod.ts
+++ apps/shell/src/environments/environment.prod.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: true,
   youtubeApiKey: 'AIzaSyCXeDGTeWD9-gVZY6VWoRPLW0LOT0yn_Es',
   backendUrl: 'https://youtube-webapp-341311.uc.r.appspot.com',
+  remotesUrl: {
+    watchApp: 'https://watch-app-b4d5f.web.app',
+    likesApp: 'https://likes-app-e1fe3.web.app'
+  }
 };
