8bae4f039d7254fdcb8e3e6a1882e9072248636c
Vugar <vugar_abdullayev@outlook.com>
2022-02-20 22:00:00 +0100

    Feature/add apps firebase config (#36)
    * add firebase config and prod app urls
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- apps/shell/src/app/registry.ts
+++ apps/shell/src/app/registry.ts
@@ -1,17 +1,19 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { APP_VERSION } from 'apps/shell/app.constants';
+import { environment } from '../environments/environment';
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export const registry: any = {
   watchApp: (): Promise<any> =>
     loadRemoteModule({
       type: 'module',
-      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      remoteEntry: `${environment.remotesUrl.watchApp}/remoteEntry.js?v=${APP_VERSION}`,
       exposedModule: './web-components',
     }),
   likesApp: (): Promise<any> =>
     loadRemoteModule({
       type: 'module',
-      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      remoteEntry: `${environment.remotesUrl.likesApp}/remoteEntry.js?v=${APP_VERSION}`,
       exposedModule: './web-components',
     }),
 };
---
+++ scripts/app-deploy.js
@@ -0,0 +1,25 @@
+const fs = require('fs-extra');
+const { execSync } = require('child_process');
+const minimist = require('minimist')
+const { rimrafPromise } = require('./utils');
+
+const appName = minimist(process.argv).app;
+if (!appName) {return;}
+
+const appDirectory = `./apps/${appName}`;
+
+(async function deploy() {
+
+  await rimrafPromise(`${appDirectory}/dist`);
+
+  await fs.ensureDir(`${appDirectory}/dist`);
+  await fs.copySync(`./dist/apps/${appName}`, `${appDirectory}/dist`, { overwrite: true} , (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`copy ${appDirectory}. Success!`);
+    }
+  });
+  console.log(appDirectory)
+  execSync(`cd ${appDirectory} && firebase deploy`);
+})();
