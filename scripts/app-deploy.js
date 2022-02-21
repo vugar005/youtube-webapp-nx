4a025cda8263255632fa35e2fbdfa26f6f0ec3fd
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-21 13:01:20 +0100

    feature/improve-material-theme

--- scripts/app-deploy.js
+++ scripts/app-deploy.js
@@ -1,25 +1,26 @@
 const fs = require('fs-extra');
 const { execSync } = require('child_process');
-const minimist = require('minimist')
+const minimist = require('minimist');
 const { rimrafPromise } = require('./utils');
 
 const appName = minimist(process.argv).app;
-if (!appName) {return;}
+if (!appName) {
+  return;
+}
 
 const appDirectory = `./apps/${appName}`;
 
 (async function deploy() {
-
   await rimrafPromise(`${appDirectory}/dist`);
 
   await fs.ensureDir(`${appDirectory}/dist`);
-  await fs.copySync(`./dist/apps/${appName}`, `${appDirectory}/dist`, { overwrite: true} , (err) => {
+  await fs.copySync(`./dist/apps/${appName}`, `${appDirectory}/dist`, { overwrite: true }, (err) => {
     if (err) {
       console.error(err);
     } else {
       console.log(`copy ${appDirectory}. Success!`);
     }
   });
-  console.log(appDirectory)
+  console.log(appDirectory);
   execSync(`cd ${appDirectory} && firebase deploy`);
 })();
