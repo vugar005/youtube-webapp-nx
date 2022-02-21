ad945307e40aec7ed2d06b9fe6102bf94319eabf
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-21 21:04:48 +0100

    feature/add-mini-video-player-mode

---
+++ apps/shell/src/app/mini-player/mini-player.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MiniPlayerComponent } from './mini-player.component';
+
+describe('MiniPlayerComponent', () => {
+  let component: MiniPlayerComponent;
+  let fixture: ComponentFixture<MiniPlayerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MiniPlayerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MiniPlayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
