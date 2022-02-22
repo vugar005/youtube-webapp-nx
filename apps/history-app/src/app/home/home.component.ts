323d71d8c5cb21cd3dc9d6cd3a7d99844ebe3b4b
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-22 13:04:37 +0100

    feature/connect-history-app-to-shell (#41)
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

---
+++ apps/history-app/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'history-app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
