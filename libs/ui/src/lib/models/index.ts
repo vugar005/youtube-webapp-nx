9623b8e79c301e11d8c4d73c5f418558e84bfb7c
Vugar <vugar_abdullayev@outlook.com>
2022-02-23 20:24:09 +0100

    feature/get-country-code (#53)
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- libs/ui/src/lib/models/index.ts
+++ libs/ui/src/lib/models/index.ts
@@ -2,3 +2,4 @@ export * from './navlink.model';
 export * from './youtube-search-list.model';
 export * from './youtube.service.model';
 export * from './mini-video-payload.model';
+export * from './country-code';
