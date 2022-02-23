26c8cc6acdbd009c5fc75fab9b6cda5dfcf56851
Vugar <vugar_abdullayev@outlook.com>
2022-02-23 20:03:54 +0100

    feature/add-mobile-searchbox (#52)
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- libs/ui/src/lib/components/index.ts
+++ libs/ui/src/lib/components/index.ts
@@ -1,5 +1,6 @@
 export * from './brand-icon';
 export * from './search-box';
+export * from './search-box-mobile';
 export * from './video-player';
 export * from './video-thumbnail';
 export * from './video-thumbnail-loader';
