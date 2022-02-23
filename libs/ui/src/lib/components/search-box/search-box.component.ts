148c3e446967ebe698dbdf791809808821ca9977
Vugar <vugar_abdullayev@outlook.com>
2022-02-23 18:35:32 +0100

    feature/improve-searchbox-behavior (#51)
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- libs/ui/src/lib/components/search-box/search-box.component.ts
+++ libs/ui/src/lib/components/search-box/search-box.component.ts
@@ -7,9 +7,12 @@ import {
   forwardRef,
   ChangeDetectorRef,
   Inject,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { debounceTime, Subject, switchMap, takeUntil } from 'rxjs';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { catchError, debounceTime, EMPTY, Observable, Subject, switchMap, takeUntil } from 'rxjs';
 import { IYoutubeService } from '../../models';
 import { IYoutubeSearchResult } from '../../models/youtube-search-list.model';
 import { YOUTUBE_SERVICE } from '../../tokens';
@@ -28,8 +31,11 @@ import { YOUTUBE_SERVICE } from '../../tokens';
   ],
 })
 export class SearchBoxComponent implements OnInit, OnDestroy, ControlValueAccessor {
+  @ViewChild('searchInput', { static: true }) searchInputRef?: ElementRef;
+  @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger }) inputAutoComplete?: MatAutocompleteTrigger;
+
   @Input() placeholder = 'Search';
-  @Input() debounceTime = 100;
+  @Input() debounceTime = 200;
   public searchControl = new FormControl();
   public searchOptions: IYoutubeSearchResult[] = [];
 
@@ -46,31 +52,40 @@ export class SearchBoxComponent implements OnInit, OnDestroy, ControlValueAccess
     this.onDestroy$.complete();
   }
 
+  // Implemented as part of ControlValueAccessor
   // eslint-disable-next-line
   public onChange(newVal: string): void {}
 
+  // Implemented as part of ControlValueAccessor
   // eslint-disable-next-line
   public onTouched(_?: any): void {}
 
+  // Implemented as part of ControlValueAccessor
   // eslint-disable-next-line
   public onValidatorChange(_?: any): void {}
 
+  // Implemented as part of ControlValueAccessor
   public writeValue(value: string): void {
     this.searchControl.patchValue(value, { emitEvent: false });
   }
 
+  // Implemented as part of ControlValueAccessor
+  // eslint-disable-next-line
   public registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
+  // Implemented as part of ControlValueAccessor
   public registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
 
+  // Implemented as part of ControlValueAccessor
   public registerOnValidatorChange(fn: () => void): void {
     this.onValidatorChange = fn;
   }
 
+  // Implemented as part of ControlValueAccessor
   public setDisabledState?(isDisabled: boolean): void {
     if (isDisabled === false) {
       this.searchControl.disable({ emitEvent: false });
@@ -79,24 +94,36 @@ export class SearchBoxComponent implements OnInit, OnDestroy, ControlValueAccess
     }
   }
 
-  public onInputEnter(): void {
+  public onSubmit(): void {
     this.onChange(this.searchControl.value);
+    this.inputAutoComplete?.closePanel();
   }
 
-  public onSearchEnter(): void {
+  public onOptionSelected(): void {
     this.onChange(this.searchControl.value);
+    setTimeout(() => {
+      this.searchInputRef?.nativeElement.blur();
+    }, 0);
+  }
+
+  public onFocusInput(): void {
+    this.inputAutoComplete?.openPanel();
   }
 
   private initFormListeners(): void {
     this.searchControl.valueChanges
       .pipe(
         debounceTime(this.debounceTime),
-        switchMap((text: string) => this.youtubeService.searchVideoResults({ query: text })),
+        switchMap((text: string) => this.getSearchRequest({ query: text })),
         takeUntil(this.onDestroy$)
       )
       .subscribe((results: IYoutubeSearchResult[]) => {
         this.searchOptions = results;
         this.cdr.detectChanges();
       });
+  }
+
+  private getSearchRequest(payload: { query: string }): Observable<IYoutubeSearchResult[]> {
+    return this.youtubeService.searchVideoResults(payload).pipe(catchError(() => EMPTY));
   }
 }
